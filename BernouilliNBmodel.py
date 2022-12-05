32cd2bc15e5260d0a55d08081b8f8d3ed4522ec1
Luigi Bozzi <77425555+bozzilui@users.noreply.github.com>
2022-12-05 18:58:01 -0500

    Add files via upload

--- BernouilliNBmodel.py
+++ BernouilliNBmodel.py
@@ -11,17 +11,20 @@ class BernoulliNB:
         self.num_classes = len(np.unique(y))
         self.num_features = X.shape[1]
 
+
         # Compute the prior probabilities for each class
         self.priors = np.zeros(self.num_classes)
         for i in range(self.num_classes):
-            self.priors[i] = np.mean(y == i)
+            self.priors[i] = np.mean(y == (i+1))
+
 
         # Compute the likelihood probabilities for each class
         self.likelihoods = np.zeros((self.num_classes, self.num_features))
         for i in range(self.num_classes):
             for j in range(self.num_features):
                 # Compute the likelihood of feature j in class i
-                self.likelihoods[i, j] = np.mean(X[y == i, j] == 1)
+                self.likelihoods[i, j] = np.mean(X[y == (i+1), j] == 1)
+        #print(self.likelihoods)
 
     # Predict the class labels for a set of samples
     def predict(self, X):
@@ -37,20 +40,27 @@ class BernoulliNB:
         for i in range(self.num_classes):
             # Compute the log prior probability for class i
             log_prior = np.log(self.priors[i])
-
+            print(num_samples)
+            print(num_features)
             # Compute the log likelihood probabilities for each feature in class i
             log_likelihoods = np.zeros(num_samples)
             for j in range(num_features):
                 if np.any(X[:, j] == 1):
-                    log_likelihoods[j] = np.log(self.likelihoods[i, j])
+                    log_likelihoods[j] = np.log(self.priors[i]+self.likelihoods[i, j])
                 else:
-                    log_likelihoods[j] = np.log(1 - self.likelihoods[i, j])
+                    log_likelihoods[j] = np.log(1 - (self.priors[i] + self.likelihoods[i, j]))
 
             # Compute the log probability for class i
-    
-            log_probs[:, i] = log_prior + log_likelihoods
+
+            log_probs[:, i] = log_likelihoods
+
+
+
 
         # Predict the class labels
-        pred = np.argmax(log_probs, axis=1)
+        for i in range(log_probs.shape[0]):
+            pred[i] = np.argmax(log_probs[i, :])
 
+
+        #return np.round(np.mean(abs(log_probs[:, 1:]), axis=1))
         return pred
